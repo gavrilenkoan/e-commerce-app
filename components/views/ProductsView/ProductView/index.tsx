fec8d24bf31e54465930b187623fd3d2b06b277b
gavrilenko_an <havrilenko.ann@gmail.com>
2026-02-18 14:59:43 +0200

    feat: product detail page

--- components/views/ProductsView/ProductView/index.tsx
+++ components/views/ProductsView/ProductView/index.tsx
@@ -1,7 +1,36 @@
 import { Product } from "@/types/product";
+import Image from "next/image"
 
 const ProductPage = ({ product }: { product: Product }) => {
-    return <div>detail</div>
+    return (
+        <div>
+            {product.images && product.images[0] && (
+                <div>
+                    <Image 
+                        alt={product.title}
+                        src={product.images[0]}
+                        width={200}
+                        height={200}
+                    />
+                </div>
+            )}
+
+            <div>
+                <h2>{product.title}</h2>
+            </div>
+
+            <div>
+                <p>{product.description}</p>
+                <h3>${product.price}</h3>
+            </div>
+
+            <div>
+                <button>-</button>
+                <span>0</span>
+                <button>+</button>
+            </div>
+        </div>
+    )
 };
 
 export default ProductPage;
